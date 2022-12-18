e7468b45a8230b60105af16f2f7ca947a4f6e864
Jonas Stettner <mail@jstet.net>
2022-12-18 21:35:50 +0100

    restructuring

---
+++ scraper/scrape_report_data.py
@@ -0,0 +1,172 @@
+import os
+import sys
+import time
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from rich.console import Console
+from rich.progress import track
+from datetime import datetime
+import warnings
+
+class NoData(Exception):
+    pass
+
+
+# initializing rich console
+console = Console()
+
+console.print("Hello :smiley:")
+console.rule("")
+
+# configuring selenium
+# setting download folder
+dl_folder = f"download"
+# setting options
+op = webdriver.ChromeOptions()
+op.add_argument('--no-sandbox')
+op.add_argument('--verbose')
+op.add_argument("--disable-notifications")
+op.add_experimental_option("prefs", {
+    "download.default_directory": dl_folder,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True})
+op.add_argument('--disable-gpu')
+op.add_argument('--disable-software-rasterizer')
+op.set_capability('unhandledPromptBehavior', 'accept')
+op.add_argument('--headless')
+
+# using selenium to automate clicking on the right parts of the navbar and downloading excel file
+def get_dfs(url, driver, dl_folder, country, year, count, maxi):
+    console.log(
+        f"trying national report {count}/{maxi}. Url: {url} belonging to {country} in year {year}")
+
+    driver.get(url)
+    
+    results = []
+    
+    for i in ["Export", "Import"]:
+        driver.implicitly_wait(2)
+        
+        dct = {}
+        dct["error"] = ""
+        try:
+            element = WebDriverWait(driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, f"//*[contains(text(), '{i} of hazardous wastes and other wastes')]")))
+        except TimeoutException:
+            raise NoData("Category button not clickable")
+
+        element.click()
+        
+        try:
+            time.sleep(4)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            element = WebDriverWait(driver, 4).until(
+            EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Export to Excel')]")))
+        except Exception as e:
+            console.log(f"Couldnt find excel button of {i}")
+            dct["error"] = "Couldnt find excel button"
+            results.append(dct)
+            continue
+    
+        element.click()
+
+        # wating some time for file dl
+        time.sleep(2)
+
+        try:
+            ex_f = os.listdir(dl_folder)[0]
+            with warnings.catch_warnings(record=True):
+                warnings.simplefilter("always")
+                temp_df = pd.read_excel(f"{dl_folder}/{ex_f}")
+        except Exception as e:
+            console.log("Something went wrong with the file")
+            console.log(e)
+            dct["error"] = "Something went wrong with the file"
+            results.append(dct)
+            continue
+        
+        for f in os.listdir(dl_folder):
+            os.remove(os.path.join(dl_folder, f))
+            
+
+        temp_df["country"] = country
+        temp_df["year"] = year
+
+        # sometimes the column names are not exported correctly
+        # also chance to rename them 
+        zero = "annex_2_8_9_code"
+        one = "annex_1_code"
+        two = "national_code"
+        three = "type_of_waste"
+        four = "annex_3_code"
+        five = "amount"
+        six = "countries_of_transit "
+        if i == "Export":
+            seven = "country_of_destination"
+        if i == "Import":
+            seven = "country_of_origin"
+        eight = "annex_4_a_code"
+        nine = "annex_4_b_code"
+
+        temp_df = temp_df.rename(columns={temp_df.columns[0]: zero,  temp_df.columns[1]: one,  temp_df.columns[2]: two,  temp_df.columns[3]: three,
+                                        temp_df.columns[4]: four,  temp_df.columns[5]: five,  temp_df.columns[6]: six,  temp_df.columns[7]: seven,  temp_df.columns[8]: eight,  temp_df.columns[9]: nine})
+        temp_df = temp_df.rename(columns=lambda x: x.replace('\n', ''))
+
+        
+        dct["df"] = temp_df
+        results.append(dct)
+        
+
+    return results[0],results[1]
+
+
+def download(reports):
+    # initializing chrome driver
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(options=op)
+    failed = []
+    frames_exports = []
+    frames_imports = []
+    for i in range(len(reports)):
+        year = reports.loc[i, "year"]
+        country = reports.loc[i, "country"]
+        link = reports.loc[i, "report_link"]
+        
+        try:
+            exports,imports = get_dfs(link, driver, dl_folder, country, year, i+1, len(reports))
+            
+        except NoData as e:
+            failed.append(
+                {"year": year, "country": country, "link": link, "reason": e})
+            print(e)
+            continue
+        
+        for count,i in enumerate([exports,imports]):
+            kind = ["exports","imports"][count]
+            if i["error"] != "":
+                 failed.append(
+                {"year": year, "country": country, "link": link, "reason": i["error"], "kind": kind})
+            else:
+                i["df"].to_csv(f"../output/single/{kind}/{year}_{country.replace(' ','_')}_{kind}.csv")
+                exec(f"frames_{kind}.append(exports['df'])")
+       
+    for i in ["exports","imports"]:
+                df = eval(f"pd.concat(frames_{i})")
+                df.to_csv(f"../output/{i}.csv", index=False)  
+        
+    failed = pd.DataFrame(failed)   
+    failed.to_csv("../output/all_failed.csv")
+
+    driver.close()
+    
+reports = pd.read_csv("../output/national_reports.csv")
+download(reports[:2])
---
+++ scraper/scrape_reports.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import time
+import re
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from rich.console import Console
+from rich.progress import track
+from datetime import datetime
+import warnings
+
+
+class NoData(Exception):
+    pass
+
+
+# initializing rich console
+console = Console()
+
+console.print("Hello :smiley:")
+console.rule("")
+
+# initializing requests session
+r_obj = requests.Session()
+
+# using beautiful soup to retreive country report overview pages for all years
+# first page
+url = "http://www.basel.int/Countries/NationalReporting/NationalReports/BC2021Reports/tabid/9379/Default.aspx"
+# selector retreived with scrape mate plugin
+css_selector = "#dnn_LeftColumn .Normal"
+r_soup = r_obj.get(url)
+soup = BeautifulSoup(r_soup.content, "html.parser")
+result_set = soup.select(css_selector)
+links = [x["href"] for x in result_set]
+
+# looping over country report overview pages for all years
+root = "http://ers.basel.int/ERS-Extended/FeedbackServer/"
+countries_all = []
+years_all = []
+submission_dates_all = []
+report_links_all = []
+for count, link in enumerate(links):
+
+    # extracting year
+    s = result_set[count].contents[0]
+    year = re.findall("\d*", s)[0]
+
+    console.log(
+        f"Trying: {link}, which is {count+1}/{len(links)}. Year: {year}")
+
+    try:
+        url = link
+        r_soup = r_obj.get(url)
+    # some links are not a real url but something like this: LinkClick.aspx?link=4751&tabid=9379&portalid=4&mid=13039
+    # so we have to manually build an url by extracting year and tab_id (whatever that is)
+    except requests.exceptions.RequestException as e:
+        console.log(f"We need to build this url.")
+        tab_id = re.findall("(?<=tabid=)(.*)(?=&portal)", link)[0]
+        url = f"http://www.basel.int/Countries/NationalReporting/NationalReports/BC{year}Reports/tabid/{tab_id}/Default.aspx"
+        r_soup = r_obj.get(url)
+
+    # the country reports are embedded in a iframe, so we cant retreive links directly with beautiful soup
+    # we need to extract the source url of the iframe and navigate to it
+    soup = BeautifulSoup(r_soup.content, "html.parser")
+    iframe_url = soup.findAll("iframe")[1]["src"]
+
+    r_soup = r_obj.get(iframe_url)
+    soup = BeautifulSoup(r_soup.content, "html.parser")
+
+    # extracting report links, corresponding countries and submission dates
+    spans = soup.findAll("span")
+    temp_links = [x.findAll("a", href=True)[1] for x in spans]
+    report_links = [f"{root}{x['href']}" for x in temp_links]
+    countries = [x.contents[0] for x in temp_links]
+    years = [year] * len(countries)
+    css_selector = "td:nth-child(5)"
+    # extracting and cleaning submission dates
+    submission_dates = [x.contents[0].replace(
+        u'\xa0', "") for x in soup.select(css_selector)]
+    submission_dates = [datetime.strptime(
+        x, "%d/%m/%Y %H:%M:%S") for x in submission_dates]
+
+    countries_all = countries_all + countries
+    years_all = years_all + years
+    submission_dates_all = submission_dates_all + submission_dates
+    report_links_all = report_links_all + report_links
+
+
+console.log("Saving national repot meta data as data frame")
+df = pd.DataFrame({"report_link": report_links_all, "country": countries_all,
+                  "submission_date": submission_dates_all, "year": years_all})
+
+df.to_csv("../output/national_reports.csv", index=False)
+
+
