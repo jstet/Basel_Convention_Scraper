d56b2a55f92bf6d9e8afcc101dc66c69f947c19a
Jonas Stettner <mail@jstet.net>
2022-12-18 15:54:07 +0100

    scraping national report meta data for all years

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import re
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -11,12 +12,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
+from rich.console import Console
+from rich.progress import track
+from datetime import datetime
 
 class NoData(Exception):
     pass
 
-dl_folder = f"{os.getcwd()}/download"
+# initializing rich console
+console = Console()
 
+console.print("Hello :smiley:")
+console.rule("")
+
+# configuring selenium
+## setting download folder
+dl_folder = f"{os.getcwd()}/download"
+## setting options
 op = webdriver.ChromeOptions()
 op.add_argument('--no-sandbox')
 op.add_argument('--verbose')
@@ -30,27 +42,86 @@ op.add_argument('--disable-gpu')
 op.add_argument('--disable-software-rasterizer')
 op.set_capability('unhandledPromptBehavior', 'accept')
 op.add_argument('--headless')
+## initializing chrome driver
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome(options=op)
 
-
+# initializing requests session
 r_obj = requests.Session()
-url = "http://ers.basel.int/ERS-Extended/FeedbackServer/webreportprint.aspx?reportid=ce3701bd61c4a2ea8d1e8fb5cb14a1e&startdate=&enddate=&filterid=38&filterlanguage=&reportlanguage="  # i.e. http://bisegrw.edu.pk/
+
+def get_soup(url, r_obj):
+    return r_obj.get(url)
+
+# using beautiful soup to retreive country report overview pages for all years
+# first page
+url = "http://www.basel.int/Countries/NationalReporting/NationalReports/BC2021Reports/tabid/9379/Default.aspx"
+# selector retreived with scrape mate plugin
+css_selector = "#dnn_LeftColumn .Normal"
 r_soup = r_obj.get(url)
-
 soup = BeautifulSoup(r_soup.content, "html.parser")
+result_set = soup.select(css_selector)
+links = [x["href"] for x in result_set]
 
-spans = soup.findAll("span")
+# looping over country report overview pages for all years
+root = "http://ers.basel.int/ERS-Extended/FeedbackServer/"
+national_reports_dcts = []
+countries_all = []
+years_all = []
+submission_dates_all = []
+report_links_all = []
+for count,link in enumerate(links): 
+    
+    # extracting year
+    s = result_set[count].contents[0]
+    year = re.findall("\d*", s)[0] 
 
-links = [x.findAll("a", href=True)[1] for x in spans]
+    console.log(f"Trying: {link}, which is {count+1}/{len(links)}. Year: {year}")  
+    
+    try:
+        url = link
+        r_soup = r_obj.get(url)
+    # some links are not a real url but something like this: LinkClick.aspx?link=4751&tabid=9379&portalid=4&mid=13039 
+    # so we have to manually build an url by extracting year and tab_id (whatever that is)
+    except requests.exceptions.RequestException as e:    
+        console.log(f"We need to build this url.")   
+        tab_id = re.findall("(?<=tabid=)(.*)(?=&portal)", link)[0]
+        url = f"http://www.basel.int/Countries/NationalReporting/NationalReports/BC{year}Reports/tabid/{tab_id}/Default.aspx"
+        r_soup = r_obj.get(url)
+    
+    # the country reports are embedded in a iframe, so we cant retreive links directly with beautiful soup
+    # we need to extract the source url of the iframe and navigate to it
+    soup = BeautifulSoup(r_soup.content, "html.parser")
+    iframe_url = soup.findAll("iframe")[1]["src"]
+    
+    r_soup = r_obj.get(iframe_url)
+    soup = BeautifulSoup(r_soup.content, "html.parser")
+    
+    # extracting report links, corresponding countries and submission dates
+    spans = soup.findAll("span")
+    temp_links = [x.findAll("a", href=True)[1] for x in spans]
+    report_links = [f"{root}{x['href']}" for x in temp_links]
+    countries = [x.contents[0] for x in temp_links]
+    css_selector = "td:nth-child(5)"
+    # extracting and cleaning submission dates
+    submission_dates = [x.contents[0].replace(u'\xa0', "") for x in soup.select(css_selector)]
+    submission_dates = [datetime.strptime(x, "%d/%m/%Y %H:%M:%S") for x in submission_dates]
+    
+    national_reports_dcts.append({"report_links":report_links,"countries": countries, "submission_dates": submission_dates})
+    
+    countries_all = countries_all + countries
+    years_all = years_all + [year] * len(countries)
+    submission_dates_all = submission_dates_all + submission_dates
+    report_links_all = report_links_all + report_links
+    
 
-countries = [x.contents[0] for x in links]
+console.log("Saving national repot meta data as data frame")
+df = pd.DataFrame({"report_link":report_links_all,"country": countries_all, "submission_date": submission_dates_all, "year": years_all})
+    
+df.to_csv("../data/national_reports.csv", index=False)
 
 root = "http://ers.basel.int/ERS-Extended/FeedbackServer/"
 
 urls = [f"{root}{x['href']}" for x in links]
-
-chromedriver_autoinstaller.install()
-
-driver = webdriver.Chrome(options=op)
 
 def get_df(url, driver, dl_folder, countries, count):
     print(f"trying url: {url} belonging to {countries[count]}")
